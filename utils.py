de0ada1b55507a1bf81fc0602e8f3bcf1bece16f
trevorwitter <trevorgwitter@gmail.com>
2023-09-11 20:33:25 -0400

    update dice loss class

--- utils.py
+++ utils.py
@@ -2,18 +2,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DiceLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, sigmoid=True):
         super(DiceLoss, self).__init__()
+        self.sigmoid = sigmoid
 
     def forward(self, preds, labels, epsilon=1):
-        preds = F.sigmoid(preds)       
-        #flatten label and prediction tensors
-        preds = preds.view(-1)
-        labels = labels.view(-1)
-        intersection = (preds * labels).sum()                            
-        dice = (2.*intersection + epsilon)/(preds.sum() + labels.sum() + epsilon)  
-        return 1 - dice
-    
+        if self.sigmoid == True:
+            preds = F.sigmoid(preds)   
+            
+        diceLabel = labels.sum(dim=[1,2,3])
+        dicePrediction = preds.sum(dim=[1,2,3])
+        diceCorrect = (preds * labels).sum(dim=[1,2,3])
+
+        diceRatio = (2. * diceCorrect + epsilon)/(dicePrediction + diceLabel + epsilon)       
+        return (1 - diceRatio).mean()
 
 if __name__ == "__main__":
     pass
